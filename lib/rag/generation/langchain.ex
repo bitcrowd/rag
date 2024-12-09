bd1cb4ec46ae10a8040218fa5f2de89e80909e87
Joel Koch <joel@bitcrowd.net>
2024-12-09 22:08:12 +0100

    Generation requires prompt as input
    We must be able to adjust the prompt used for generation.
    For instance, different LLMs work best with different prompt formats.
    We can also support other languages than English that way.
    
    With this commit you must build the prompt independently from the
    generation and pass it to the generation step.

--- lib/rag/generation/langchain.ex
+++ lib/rag/generation/langchain.ex
@@ -6,41 +6,18 @@ defmodule Rag.Generation.LangChain do
   alias LangChain.Message
 
   @doc """
-  Takes the values of `query` and `query_results` from `rag_state`.
-  Creates a context from `query_results` and injects `query` and the context into a prompt which is then passed to to an LLM specified by `chain` to generate a response.
-  Then, puts `context`, `context_sources`, and `response` in `rag_state`.
+  Passes `prompt` from `rag_state` to an LLM specified by `chain` to generate a response.
+  Then, puts `response` in `rag_state`.
   """
-  @spec generate_response(
-          %{query: String.t(), query_results: %{document: String.t(), source: String.t()}},
-          LLMChain.t()
-        ) :: %{context: String.t(), context_sources: list(String.t()), response: String.t()}
+  @spec generate_response(%{prompt: String.t()}, LLMChain.t()) :: %{response: String.t()}
   def generate_response(rag_state, chain) do
-    %{query: query, query_results: query_results} = rag_state
-
-    {context, context_sources} =
-      query_results |> Enum.map(&{&1.document, &1.source}) |> Enum.unzip()
-
-    context = Enum.join(context, "\n\n")
-
-    prompt =
-      """
-      Context information is below.
-      ---------------------
-      #{context}
-      ---------------------
-      Given the context information and no prior knowledge, answer the query.
-      Query: #{query}
-      Answer:
-      """
+    %{prompt: prompt} = rag_state
 
     {:ok, _updated_chain, response} =
       chain
       |> LLMChain.add_message(Message.new_user!(prompt))
       |> LLMChain.run()
 
-    rag_state
-    |> Map.put(:context, context)
-    |> Map.put(:context_sources, context_sources)
-    |> Map.put(:response, response.content)
+    Map.put(rag_state, :response, response.content)
   end
 end
--- test/rag/generation/nx_test.exs
+++ test/rag/generation/nx_test.exs
@@ -5,61 +5,23 @@ defmodule Rag.Generation.NxTest do
   alias Rag.Generation
 
   describe "generate_response/2" do
-    test "injects documents as context into the prompt" do
-      expect(Nx.Serving, :batched_run, fn _serving, prompt -> %{results: [%{text: prompt}]} end)
+    test "calls `serving` with prompt to generate a response" do
+      expect(Nx.Serving, :batched_run, fn serving, prompt ->
+        assert serving == TestServing
+        assert prompt == "a prompt"
+        %{results: [%{text: "a response"}]}
+      end)
 
-      query = "What is RAG?"
+      prompt = "a prompt"
 
-      query_results = [
-        %{source: "first_document", document: "RAG stands for Retrieval Augmented Generation"},
-        %{source: "second_document", document: "RAG systems are complex"}
-      ]
+      rag_state = %{prompt: prompt}
 
-      assert %{
-               context:
-                 "RAG stands for Retrieval Augmented Generation\n\nRAG systems are complex",
-               context_sources: ["first_document", "second_document"],
-               response: """
-               <|system|>
-               You are a helpful assistant.</s>
-               <|user|>
-               Context information is below.
-               ---------------------
-               RAG stands for Retrieval Augmented Generation
-
-               RAG systems are complex
-               ---------------------
-               Given the context information and no prior knowledge, answer the query.
-               Query: What is RAG?
-               Answer: </s>
-               <|assistant|>
-               """
-             } =
-               Generation.Nx.generate_response(%{
-                 query: query,
-                 query_results: query_results
-               })
+      assert %{response: "a response"} = Generation.Nx.generate_response(rag_state, TestServing)
     end
 
-    test "errors if query or query_results not present" do
+    test "errors if prompt not present" do
       assert_raise MatchError, fn ->
-        Generation.Nx.generate_response(%{query: "hello"})
-      end
-
-      assert_raise MatchError, fn ->
-        Generation.Nx.generate_response(%{query_results: []})
-      end
-    end
-
-    test "errors if query_results don't have document or source key" do
-      query = "What is RAG?"
-
-      query_results = [
-        %{id: "first_document", text: "RAG stands for Retrieval Augmented Generation"}
-      ]
-
-      assert_raise KeyError, fn ->
-        Generation.Nx.generate_response(%{query: query, query_results: query_results})
+        Generation.Nx.generate_response(%{})
       end
     end
   end
