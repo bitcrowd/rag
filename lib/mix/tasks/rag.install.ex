2a29f7cf58de8b84a6d452d3cff08a99c0053676
Joel Koch <joel@bitcrowd.net>
2024-12-20 11:42:56 +0100

    Update installer
    - keep only two options for vector store: pgvector and chroma
    - use only dependencies from hex.pm
    - fix installer generated code to work with Rag.Generation struct
    
    Use smaller models in installer to make it easier to get started
    - embeddings model: thenlper/gte-small
    - llm: HuggingFaceTB/SmolLM2-135M-Instruct

--- lib/mix/tasks/rag.install.ex
+++ lib/mix/tasks/rag.install.ex
@@ -56,43 +56,33 @@ defmodule Mix.Tasks.Rag.Install do
 
     igniter =
       igniter
-      |> Igniter.Project.Deps.add_dep(
-        {:bumblebee, github: "joelpaulkoch/bumblebee", branch: "jina-embeddings-v2"}
-      )
       |> Igniter.Project.Deps.add_dep({:langchain, "~> 0.3.0-rc.0"})
       |> Igniter.Project.Deps.add_dep({:text_chunker, "~> 0.3.1"})
+      |> Igniter.Project.Deps.add_dep({:bumblebee, "~> 0.6.0"})
+      |> Igniter.Project.Deps.add_dep({:axon, "~> 0.7.0"})
+      |> Igniter.Project.Deps.add_dep({:exla, "~> 0.9.1"})
       |> Igniter.Project.Deps.add_dep({:nx, "~> 0.9.0"})
       |> Igniter.Project.Config.configure("config.exs", :nx, [:default_backend], EXLA.Backend)
-      |> Igniter.Project.Deps.add_dep({:exla, "~> 0.9.1"})
-      |> Igniter.Project.Deps.add_dep({:axon, "~> 0.7.0"})
 
     case vector_store do
-      "pgvector" -> add_with_pgvector(igniter)
-      "sqlite_vec" -> add_with_sqlite_vec(igniter)
-      "chroma" -> add_with_chroma(igniter)
+      "pgvector" ->
+        with_pgvector(igniter)
+
+      "chroma" ->
+        with_chroma(igniter)
+
+      _other ->
+        raise "Only pgvector and chroma are supported. Run `mix rag.install pgvector` or `mix rag.install chroma`"
     end
   end
 
-  defp add_with_chroma(igniter) do
+  defp with_chroma(igniter) do
     igniter
     |> Igniter.Project.Deps.add_dep({:chroma, "~> 0.1.3"})
     |> Igniter.apply_and_fetch_dependencies()
     |> add_config(:chroma)
     |> add_servings()
     |> add_rag_module(:chroma)
-  end
-
-  defp add_with_sqlite_vec(igniter) do
-    igniter
-    |> Igniter.Project.Deps.add_dep({:ecto, "~> 3.12"})
-    |> Igniter.Project.Deps.add_dep({:ecto_sql, "~> 3.12"})
-    |> Igniter.Project.Deps.add_dep({:sqlite_vec, "~> 0.1.0", override: true})
-    |> Igniter.apply_and_fetch_dependencies()
-    |> add_config(:sqlite_vec)
-    |> add_schema(:sqlite_vec)
-    |> add_migration(:sqlite_vec)
-    |> add_servings()
-    |> add_rag_module(:sqlite_vec)
   end
 
   defp add_config(igniter, :chroma) do
@@ -102,30 +92,10 @@ defmodule Mix.Tasks.Rag.Install do
     |> Igniter.Project.Config.configure("config.exs", :chroma, [:api_version], "v1")
   end
 
-  defp add_config(igniter, :sqlite_vec) do
-    app_name = Igniter.Project.Application.app_name(igniter)
-
-    root_module =
-      app_name
-      |> to_string()
-      |> Macro.camelize()
-
-    repo_module = Module.concat(root_module, "Repo")
-
+  defp with_pgvector(igniter) do
     igniter
-    |> Igniter.Project.Config.configure("config.exs", :sqlite_vec, [:version], "0.1.5")
-    |> Igniter.Project.Config.configure(
-      "runtime.exs",
-      app_name,
-      [repo_module, :load_extensions],
-      {:code, Sourceror.parse_string!("[SqliteVec.path()]")}
-    )
-  end
-
-  defp add_with_pgvector(igniter) do
-    igniter
-    |> Igniter.Project.Deps.add_dep({:ecto, "~> 3.12"})
-    |> Igniter.Project.Deps.add_dep({:ecto_sql, "~> 3.12"})
+    |> Igniter.Project.Deps.add_dep({:ecto, "~> 3.0"})
+    |> Igniter.Project.Deps.add_dep({:ecto_sql, "~> 3.10"})
     |> Igniter.Project.Deps.add_dep({:pgvector, "~> 0.3.0"})
     |> Igniter.apply_and_fetch_dependencies()
     |> add_postgrex_types()
@@ -193,38 +163,6 @@ defmodule Mix.Tasks.Rag.Install do
     )
   end
 
-  defp add_schema(igniter, :sqlite_vec) do
-    app_name = Igniter.Project.Application.app_name(igniter)
-
-    root_module =
-      app_name
-      |> to_string()
-      |> Macro.camelize()
-
-    schema_module = Module.concat(root_module, "Rag.Chunk")
-
-    Igniter.Project.Module.create_module(
-      igniter,
-      schema_module,
-      """
-      use Ecto.Schema
-
-      schema "chunks" do
-        field(:document, :string)
-        field(:source, :string)
-        field(:chunk, :string)
-        field(:embedding, SqliteVec.Ecto.Float32)
-
-        timestamps()
-      end
-
-      def changeset(chunk \\\\ %__MODULE__{}, attrs) do
-        Ecto.Changeset.cast(chunk, attrs, [:document, :source, :chunk, :embedding])
-      end
-      """
-    )
-  end
-
   defp add_migration(igniter, :pgvector) do
     app_name = Igniter.Project.Application.app_name(igniter)
 
@@ -246,7 +184,7 @@ defmodule Mix.Tasks.Rag.Install do
           add(:document, :text)
           add(:source, :text)
           add(:chunk, :text)
-          add(:embedding, :vector, size: 768)
+          add(:embedding, :vector, size: 384)
 
           timestamps()
         end
@@ -255,36 +193,6 @@ defmodule Mix.Tasks.Rag.Install do
       def down() do
         drop(table(:chunks))
         execute("DROP EXTENSION vector")
-      end
-      """
-    )
-  end
-
-  defp add_migration(igniter, :sqlite_vec) do
-    app_name = Igniter.Project.Application.app_name(igniter)
-
-    root_module =
-      app_name
-      |> to_string()
-      |> Macro.camelize()
-
-    repo_module = Module.concat(root_module, "Repo")
-
-    Igniter.Libs.Ecto.gen_migration(igniter, repo_module, "add_chunks_table",
-      body: """
-      def up() do
-        create table(:chunks) do
-          add(:document, :text)
-          add(:source, :text)
-          add(:chunk, :text)
-          add(:embedding, :binary)
-
-          timestamps()
-        end
-      end
-
-      def down() do
-        drop(table(:chunks))
       end
       """
     )
@@ -305,13 +213,9 @@ defmodule Mix.Tasks.Rag.Install do
       servings_module,
       """
       def build_embedding_serving() do
-        repo = {:hf, "jinaai/jina-embeddings-v2-base-en"}
+        repo = {:hf, "thenlper/gte-small"}
 
-        {:ok, model_info} =
-          Bumblebee.load_model(repo,
-            spec_overrides: [architecture: :base],
-            params_filename: "model.safetensors"
-          )
+        {:ok, model_info} = Bumblebee.load_model(repo)
 
         {:ok, tokenizer} = Bumblebee.load_tokenizer(repo)
 
@@ -324,7 +228,7 @@ defmodule Mix.Tasks.Rag.Install do
       end
 
       def build_llm_serving() do
-        repo = {:hf, "microsoft/phi-3.5-mini-instruct"}
+        repo = {:hf, "HuggingFaceTB/SmolLM2-135M-Instruct"}
 
         {:ok, model_info} = Bumblebee.load_model(repo)
         {:ok, tokenizer} = Bumblebee.load_tokenizer(repo)
@@ -385,19 +289,29 @@ defmodule Mix.Tasks.Rag.Install do
       import Ecto.Query
       import Pgvector.Ecto.Query
 
+      def ingest(path) do
+        path
+        |> load()
+        |> index()
+      end
+
+      def load(path) do
+        path
+        |> list_text_files()
+        |> Enum.map(&%{source: &1})
+        |> Enum.map(&Rag.Loading.load_file(&1))
+      end
+
       defp list_text_files(path) do
-      path
+        path
         |> Path.join("/**/*.txt")
         |> Path.wildcard()
       end
 
-      def ingest(path) do
+      def index(ingestions) do
         chunks =
-          path
-          |> list_text_files()
-          |> Enum.map(&%{source: &1})
-          |> Enum.map(&Rag.Loading.load_file(&1))
-          |> Enum.flat_map(&Rag.Loading.chunk_text(&1))
+          ingestions
+          |> Enum.flat_map(&Rag.Loading.chunk_text(&1, :document))
           |> Rag.Embedding.Nx.generate_embeddings_batch(:chunk, :embedding)
           |> Enum.map(&to_chunk(&1))
 
@@ -405,101 +319,80 @@ defmodule Mix.Tasks.Rag.Install do
       end
 
       def query(query) do
-        %{query: query}
-        |> Rag.Embedding.Nx.generate_embedding(:query, :query_embedding)
-        |> query_with_pgvector()
-        |> Rag.Generation.Nx.generate_response()
+        generation =
+          Rag.Generation.new(query)
+          |> Rag.Embedding.Nx.generate_embedding()
+          |> Rag.Retrieval.retrieve(:fulltext_results, fn generation -> query_fulltext(generation) end)
+          |> Rag.Retrieval.retrieve(:semantic_results, fn generation ->
+            query_with_pgvector(generation)
+          end)
+          |> Rag.Retrieval.reciprocal_rank_fusion(
+            %{fulltext_results: 1, semantic_results: 1},
+            :rrf_result
+          )
+
+        context =
+          Rag.Generation.get_retrieval_result(generation, :rrf_result)
+          |> Enum.map_join("\n\n", & &1.document)
+
+        context_sources =
+          Rag.Generation.get_retrieval_result(generation, :rrf_result)
+          |> Enum.map(& &1.source)
+
+        prompt = smollm_prompt(query, context)
+
+        generation = %{
+          generation
+          | context: context,
+            context_sources: context_sources,
+            prompt: prompt
+        }
+
+        Rag.Generation.Nx.generate_response(generation)
       end
 
-      defp to_chunk(input) do
+      defp to_chunk(ingestion) do
         now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
 
-        input
-        |> Map.take([:document, :source, :chunk, :embedding])
+        ingestion
         |> Map.put_new(:inserted_at, now)
         |> Map.put_new(:updated_at, now)
       end
 
-      defp query_with_pgvector(%{query_embedding: query_embedding} = input, limit \\\\ 3) do
-        results =
-          Repo.all(
-            from(c in #{inspect(schema_module)},
-              order_by: l2_distance(c.embedding, ^Pgvector.new(query_embedding)),
-              limit: ^limit
-            )
+      defp query_with_pgvector(%{query_embedding: query_embedding}, limit \\\\ 3) do
+        Repo.all(
+          from(c in #{inspect(schema_module)},
+            order_by: l2_distance(c.embedding, ^Pgvector.new(query_embedding)),
+            limit: ^limit
           )
-
-       Map.put(input, :query_results, results)
-      end
-      """
-    )
-  end
-
-  defp add_rag_module(igniter, :sqlite_vec) do
-    app_name = Igniter.Project.Application.app_name(igniter)
-
-    root_module =
-      app_name
-      |> to_string()
-      |> Macro.camelize()
-
-    rag_module = Module.concat(root_module, "Rag")
-    repo_module = Module.concat(root_module, "Repo")
-    schema_module = Module.concat(root_module, "Rag.Chunk")
-
-    Igniter.Project.Module.create_module(
-      igniter,
-      rag_module,
-      """
-      alias #{inspect(repo_module)}
-      import Ecto.Query
-      import SqliteVec.Ecto.Query
-
-      defp list_text_files(path) do
-        path
-          |> Path.join("/**/*.txt")
-          |> Path.wildcard()
+        )
       end
 
-      def ingest(path) do
-        chunks =
-          path
-          |> list_text_files()
-          |> Enum.map(&%{source: &1})
-          |> Enum.map(&Rag.Loading.load_file(&1))
-          |> Enum.flat_map(&Rag.Loading.chunk_text(&1))
-          |> Rag.Embedding.Nx.generate_embeddings_batch(:chunk, :embedding)
-          |> Enum.map(&to_chunk(&1))
+      defp query_fulltext(%{query: query}, limit \\\\ 3) do
+        query = String.replace(query, " ", " & ")
 
-        Repo.insert_all(#{inspect(schema_module)}, chunks)
-      end
-
-      def query(query) do
-        %{query: query}
-        |> Rag.Embedding.Nx.generate_embedding(:query, :query_embedding)
-        |> query_with_sqlite_vec()
-        |> Rag.Generation.Nx.generate_response()
-      end
-
-      defp to_chunk(input) do
-        now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
-
-        input
-        |> Map.take([:document, :source, :chunk, :embedding])
-        |> Map.put_new(:inserted_at, now)
-        |> Map.put_new(:updated_at, now)
-      end
-
-      defp query_with_sqlite_vec(%{query_embedding: query_embedding} = input, limit \\\\ 3) do
-        results =
-          Repo.all(
-            from(c in #{inspect(schema_module)},
-              order_by: vec_distance_L2(c.embedding, vec_f32(SqliteVec.Float32.new(query_embedding))),
-              limit: ^limit
-            )
+        Repo.all(
+          from(c in #{inspect(schema_module)},
+            where: fragment("to_tsvector(?) @@ to_tsquery(?)", c.document, ^query),
+            limit: ^limit
           )
+        )
+      end
 
-        Map.put(input, :query_results, results)
+      defp smollm_prompt(query, context) do
+        \"""
+        <|im_start|>system
+        You are a helpful assistant.<|im_end|>
+        <|im_start|>user
+        Context information is below.
+        ---------------------
+        \#{context}
+        ---------------------
+        Given the context information and no prior knowledge, answer the query.
+        Query: \#{query}
+        Answer: <|im_end|>
+        <|im_start|>assist
+        \"""
       end
       """
     )
@@ -519,60 +412,38 @@ defmodule Mix.Tasks.Rag.Install do
       igniter,
       rag_module,
       """
-      defp list_text_files(path) do
+      def ingest(path) do
         path
-          |> Path.join("/**/*.txt")
-          |> Path.wildcard()
+        |> load()
+        |> index()
       end
 
-      def ingest(path) do
-        {:ok, collection} = Chroma.Collection.get_or_create("rag", %{"hnsw:space" => "l2"})
-
+      def load(path) do
         path
         |> list_text_files()
         |> Enum.map(&%{source: &1})
         |> Enum.map(&Rag.Loading.load_file(&1))
-        |> Enum.flat_map(&Rag.Loading.chunk_text(&1))
-        |> Rag.Embedding.Nx.generate_embeddings_batch(:chunk, :embedding)
-        |> insert_all_with_chroma(collection)
       end
 
-      def query(query) do
+      defp list_text_files(path) do
+        path
+        |> Path.join("/**/*.txt")
+        |> Path.wildcard()
+      end
+
+      def index(ingestions) do
         {:ok, collection} = Chroma.Collection.get_or_create("rag", %{"hnsw:space" => "l2"})
 
-        %{query: query}
-        |> Rag.Embedding.Nx.generate_embedding(:query, :query_embedding)
-        |> query_with_chroma(collection)
-        |> Rag.Generation.Nx.generate_response()
+        chunks =
+          ingestions
+          |> Enum.flat_map(&Rag.Loading.chunk_text(&1, :document))
+          |> Rag.Embedding.Nx.generate_embeddings_batch(:chunk, :embedding)
+
+        insert_all_with_chroma(collection, chunks)
       end
 
-      defp insert_all_with_chroma(rag_state_list, collection) do
-        batch = to_chroma_batch(rag_state_list)
-
-        Chroma.Collection.add(collection, batch)
-      end
-
-      defp query_with_chroma(rag_state, collection, limit \\\\ 3) do
-        %{query_embedding: query_embedding} = rag_state
-
-        {:ok, results} =
-          Chroma.Collection.query(collection,
-            results: limit,
-            query_embeddings: [query_embedding]
-          )
-
-        {documents, sources} = {hd(results["documents"]), hd(results["ids"])}
-
-        results =
-          Enum.zip(documents, sources)
-          |> Enum.map(fn {document, source} -> %{document: document, source: source} end)
-
-        Map.put(rag_state, :query_results, results)
-      end
-
-      defp to_chroma_batch(rag_state_list) do
-        for %{document: document, source: source, chunk: chunk, embedding: embedding} <-
-              rag_state_list,
+      defp insert_all_with_chroma(collection, ingestions) do
+        batch = for %{document: document, source: source, chunk: chunk, embedding: embedding} <- ingestions,
             reduce: %{documents: [], ids: [], sources: [], chunks: [], embeddings: []} do
           %{documents: documents, sources: sources, chunks: chunks, embeddings: embeddings} ->
             %{
@@ -584,7 +455,69 @@ defmodule Mix.Tasks.Rag.Install do
             }
         end
         |> Map.drop([:sources, :chunks])
+
+        Chroma.Collection.add(collection, batch)
       end
+
+      def query(query) do
+        {:ok, collection} = Chroma.Collection.get_or_create("rag", %{"hnsw:space" => "l2"})
+
+        generation =
+          Rag.Generation.new(query)
+          |> Rag.Embedding.Nx.generate_embedding(:query, :query_embedding)
+          |> Rag.Retrieval.retrieve(:chroma, fn generation -> query_with_chroma(collection, generation) end)
+
+        context =
+          Rag.Generation.get_retrieval_result(generation, :chroma)
+          |> Enum.map_join("\\n\\n", & &1.document)
+
+        context_sources =
+          Rag.Generation.get_retrieval_result(generation, :chroma)
+          |> Enum.map(& &1.source)
+
+        prompt = smollm_prompt(query, context)
+
+        generation = %{
+          generation
+          | context: context,
+            context_sources: context_sources,
+            prompt: prompt
+        }
+
+        Rag.Generation.Nx.generate_response(generation)
+      end
+
+      defp query_with_chroma(collection, generation, limit \\\\ 3) do
+        %{query_embedding: query_embedding} = generation
+
+        {:ok, results} =
+          Chroma.Collection.query(collection,
+            results: limit,
+            query_embeddings: [query_embedding]
+          )
+
+        {documents, sources} = {hd(results["documents"]), hd(results["ids"])}
+
+        Enum.zip_with(documents, sources, fn document, source -> %{document: document, source: source} end)
+      end
+
+
+      defp smollm_prompt(query, context) do
+        \"""
+        <|im_start|>system
+        You are a helpful assistant.<|im_end|>
+        <|im_start|>user
+        Context information is below.
+        ---------------------
+        \#{context}
+        ---------------------
+        Given the context information and no prior knowledge, answer the query.
+        Query: \#{query}
+        Answer: <|im_end|>
+        <|im_start|>assist
+        \"""
+      end
+
       """
     )
   end
